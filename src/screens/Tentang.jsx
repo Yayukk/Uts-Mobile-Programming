eadfa610e153551ec226718f5a49e0b495edb894
Yayukk <yayukoktafiana27@gmail.com>
2024-05-04 21:16:22 +0700

    Menambah tampian tentang

--- src/screens/Tentang.jsx
+++ src/screens/Tentang.jsx
@@ -1,14 +1,66 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View, TouchableOpacity} from 'react-native';
 import React from 'react';
 
-const Tentang = () => {
+const Tentang = ({navigation}) => {
   return (
-    <View>
-      <Text>Tentang</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Tentang Kami</Text>
+      <Text style={styles.description}>
+        Kami adalah perusahaan yang berfokus pada produk-produk kecantikan alami
+        dan ramah lingkungan. Tujuan kami adalah memberikan produk berkualitas
+        tinggi yang memperhatikan kesehatan kulit dan juga keberlanjutan
+        lingkungan.
+      </Text>
+      <View>
+        <TouchableOpacity
+          onPress={() => navigation.navigate('Beranda')}
+          style={styles.btn}>
+          <Text style={styles.txt}>Ke Beranda</Text>
+        </TouchableOpacity>
+      </View>
+      <View>
+        <TouchableOpacity
+          onPress={() => navigation.navigate('Favorit')}
+          style={styles.btn}>
+          <Text style={styles.txt}>Ke Favorit</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
 
 export default Tentang;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingHorizontal: 20,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 10,
+    color: '#333',
+  },
+  description: {
+    fontSize: 16,
+    textAlign: 'center',
+    color: '#666',
+  },
+  btn: {
+    margin: 10,
+    padding: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 10,
+    backgroundColor: 'pink',
+  },
+  txt: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: 'black',
+  },
+});
